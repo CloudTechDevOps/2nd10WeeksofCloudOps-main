e4409470c743f679555756278a23d9f42ac6034b
CloudTechDevOps <147053314+CloudTechDevOps@users.noreply.github.com>
2024-09-09 14:44:47 +0530

    Update vpc.tf

--- terraform_main_ec2/vpc.tf
+++ terraform_main_ec2/vpc.tf
@@ -60,7 +60,7 @@ resource "aws_security_group" "security-group" {
   description = "Allowing Jenkins, Sonarqube, SSH Access"
 
   ingress = [
-    for port in [22, 8080, 9000, 9090, 80] : {
+    for port in [22, 8080, 9000, 9090, 3306, 80] : {
       description      = "TLS from VPC"
       from_port        = port
       to_port          = port
