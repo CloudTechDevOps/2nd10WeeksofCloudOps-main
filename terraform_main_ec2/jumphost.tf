4e2c1fa971d74c927fe8a716249dc4d33e232f1d
CloudTechDevOps <147053314+CloudTechDevOps@users.noreply.github.com>
2024-09-09 14:44:04 +0530

    Update jumphost.tf

--- terraform_main_ec2/jumphost.tf
+++ terraform_main_ec2/jumphost.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "ec2" {
   ami                    = var.ami_id
   instance_type          = var.instance_type
   key_name               = var.key_name
-  subnet_id              = aws_subnet.public-subnet.id
+  subnet_id              = aws_subnet.public-subnet1.id
   vpc_security_group_ids = [aws_security_group.security-group.id]
   iam_instance_profile = aws_iam_instance_profile.instance-profile.name
   root_block_device {
