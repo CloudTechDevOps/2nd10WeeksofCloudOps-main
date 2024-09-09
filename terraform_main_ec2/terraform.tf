63a7a2a7355e6a85bb0c93c2cf89d0e7eed1b61d
CloudTechDevOps <147053314+CloudTechDevOps@users.noreply.github.com>
2024-09-09 09:21:46 +0530

    Update terraform.tf

--- terraform_main_ec2/terraform.tf
+++ terraform_main_ec2/terraform.tf
@@ -7,9 +7,9 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "asdfghjkjhgfdsdfgh"
-    key    = "terraform.tfstate"
-    region = "us-east-2"
+    bucket = "nareshawsdevv"
+    key    = "ec2/terraform.tfstate"
+    region = "us-east-1"
   }
 
   required_version = ">= 1.6.3"
