5051e354fa1e13c2b204246efe309219ada9e234
Avanthirao <avanthimirupala02@gmail.com>
2024-09-24 11:52:15 -0400

    Update config.js

--- client/src/pages/config.js
+++ client/src/pages/config.js
@@ -1,3 +1,3 @@
 // const API_BASE_URL = "http://localhost:8800";
-const API_BASE_URL = "http://api.narni.co.in";
+const API_BASE_URL = "http://api.avanthi.online.co.in";
 export default API_BASE_URL;
