69dbe33ef005d1aadff70cc1153f4c43503ab281
CloudTechDevOps <147053314+CloudTechDevOps@users.noreply.github.com>
2024-12-13 22:19:03 +0530

    Update test.sql

--- backend/test.sql
+++ backend/test.sql
@@ -1,7 +1,6 @@
 -- phpMyAdmin SQL Dump
 -- version 5.2.0
 -- https://www.phpmyadmin.net/
---
 -- Host: 127.0.0.1
 -- Generation Time: May 10, 2023 at 11:27 AM
 -- Server version: 10.4.27-MariaDB
@@ -11,23 +10,14 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
+-- Create the database if it doesn't exist
+CREATE DATABASE IF NOT EXISTS test;
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+-- Use the test database
+USE test;
 
---
--- Database: `test`
---
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `books`
---
-
-CREATE TABLE `books` (
+CREATE TABLE IF NOT EXISTS `books` (
   `id` int(11) NOT NULL,
   `title` varchar(300) NOT NULL,
   `desc` varchar(500) NOT NULL,
@@ -35,35 +25,17 @@ CREATE TABLE `books` (
   `cover` varchar(500) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
---
 -- Dumping data for table `books`
---
-
 INSERT INTO `books` (`id`, `title`, `desc`, `price`, `cover`) VALUES
 (1, 'Gamer of throne', 'this is an amazing book to read when you are free ', 2343.2, 'https://images.unsplash.com/photo-1541963463532-d68292c34b19?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mnx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60'),
-(2, 'Fire folks', 'fire folks is ming blowing book to read it will blow your mind', 2342.3, 'https://images.unsplash.com/photo-1543002588-bfa74002ed7e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80');
+(2, 'Fire folks', 'fire folks is a mind-blowing book to read; it will blow your mind', 2342.3, 'https://images.unsplash.com/photo-1543002588-bfa74002ed7e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80');
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `books`
---
+-- Add primary key for the books table
 ALTER TABLE `books`
   ADD PRIMARY KEY (`id`);
 
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `books`
---
+-- Set auto increment for the `id` column
 ALTER TABLE `books`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+COMMIT;
