191bebe45e8007b210ea2f3f9b13204b779aade1
sekhar9346 <anakalasekhar004@gmail.com>
2025-01-31 10:43:27 +0530

    Update backend.tf

--- eks-terraform/backend.tf
+++ eks-terraform/backend.tf
@@ -7,9 +7,9 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "nareshawsdevvvv"
+    bucket = "qasddcdwd"
     key    = "eks/terraform.tfstate"
-    region = "us-east-1"
+    region = "eu-west-1"
   }
 
   required_version = ">= 1.6.3"
