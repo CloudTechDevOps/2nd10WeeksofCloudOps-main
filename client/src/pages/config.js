248625e3319c03af30d77e14bc443c1bb91ea8dd
CloudTechDevOps <147053314+CloudTechDevOps@users.noreply.github.com>
2025-05-05 10:25:50 +0530

    Update config.js

--- client/src/pages/config.js
+++ client/src/pages/config.js
@@ -3,6 +3,6 @@
 // export default API_BASE_URL;
 // const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://backend";
 // export default API_BASE_URL;
-const API_BASE_URL = "REACT_APP_API_BASE107.21.73.198_URL_PLACEHOLDER";
+const API_BASE_URL = "REACT_APP_API_BASE_URL_PLACEHOLDER";
 export default API_BASE_URL;
 
