7da9d3d99c8644a6de2e6b633f4079060b78ea67
CloudTechDevOps <147053314+CloudTechDevOps@users.noreply.github.com>
2025-05-05 10:23:23 +0530

    Update config.js

--- client/src/pages/config.js
+++ client/src/pages/config.js
@@ -1,8 +1,8 @@
-const API_BASE_URL = "http://3.84.145.194:84";
+// const API_BASE_URL = "http://3.84.145.194:84";
 // const API_BASE_URL = "http://veera.narni.co.in";
 // export default API_BASE_URL;
 // const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://backend";
 // export default API_BASE_URL;
-// const API_BASE_URL = "REACT_APP_API_BASE107.21.73.198_URL_PLACEHOLDER";
+const API_BASE_URL = "REACT_APP_API_BASE107.21.73.198_URL_PLACEHOLDER";
 export default API_BASE_URL;
 
