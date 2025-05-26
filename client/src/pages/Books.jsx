933277e8044e6a3feb785e005e112a22aa5cd1c9
CloudTechDevOps <147053314+CloudTechDevOps@users.noreply.github.com>
2025-05-26 19:50:56 +0530

    Update Books.jsx

--- client/src/pages/Books.jsx
+++ client/src/pages/Books.jsx
@@ -33,7 +33,7 @@ const Books = () => {
 
   return (
     <div>
-      <h1>MultiCloud with DevOps Training by VEERA Nareshit </h1>
+      <h1>Welcomt to MultiCloud with DevOps Training by VEERA Nareshit </h1>
       <div className="books">
         {books.map((book) => (
           <div key={book.id} className="book">
