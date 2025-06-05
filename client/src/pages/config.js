e7acecc89332176aac928311dd11c15cd7c4dfc7
CloudTechDevOps <147053314+CloudTechDevOps@users.noreply.github.com>
2025-06-05 11:24:13 +0530

    Update config.js

--- client/src/pages/config.js
+++ client/src/pages/config.js
@@ -1,5 +1,5 @@
 // const API_BASE_URL = "http://3.87.228.149:84";
-const API_BASE_URL = "http://veera.905418452515.realhandsonlabs.net";
+const API_BASE_URL = "http://veera.654654232198.realhandsonlabs.net";
 // export default API_BASE_URL;
 // const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://backend";
 /// export default API_BASE_URL;
