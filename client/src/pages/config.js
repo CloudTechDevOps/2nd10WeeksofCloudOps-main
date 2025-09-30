bbbc6f6d2961ecf225a849a48d2f00f58313f3fe
CloudTechDevOps <147053314+CloudTechDevOps@users.noreply.github.com>
2025-09-30 09:34:50 +0530

    Update config.js

--- client/src/pages/config.js
+++ client/src/pages/config.js
@@ -1,5 +1,5 @@
-//const API_BASE_URL = "http://18.215.151.169:84";
-const API_BASE_URL = "http://backend.515800584282.realhandsonlabs.net";
+const API_BASE_URL = "http://3.80.67.98:86";
+//const API_BASE_URL = "http://backend.515800584282.realhandsonlabs.net";
 // export default API_BASE_URL;
 // const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://backend";
 /// export default API_BASE_URL;
