7412b9cb95f6dfda17625b8924c09c05256f2c44
CloudTechDevOps <147053314+CloudTechDevOps@users.noreply.github.com>
2025-11-01 10:35:53 +0530

    Update main.tf

--- eks-terraform/main.tf
+++ eks-terraform/main.tf
@@ -166,7 +166,7 @@ data "aws_security_group" "selected" {
     subnet_ids      = [data.aws_subnet.subnet-1.id, data.aws_subnet.subnet-2.id]
     capacity_type   = "ON_DEMAND"
     disk_size       = 20
-    instance_types  = ["t2.small"]
+    instance_types  = ["t2.micro"]
 
    
 
