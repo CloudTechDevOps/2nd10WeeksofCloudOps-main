53cae2e682ca3314a3a6d024c64927d8dee6e12d
CloudTechDevOps <147053314+CloudTechDevOps@users.noreply.github.com>
2025-12-15 12:24:03 +0530

    Update config.js

--- client/src/pages/config.js
+++ client/src/pages/config.js
@@ -1,8 +1,8 @@
-const API_BASE_URL = "http://98.92.112.243:84"
+const API_BASE_URL = "/api";  // For reverse proxy it is mandatory so dont change
+
+// const API_BASE_URL = "http://98.92.112.243:84"
 //const API_BASE_URL = "http://backend.515800584282.realhandsonlabs.net";
-// export default API_BASE_URL;
 // const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://backend";
-/// export default API_BASE_URL;
 //const API_BASE_URL = "REACT_APP_API_BASE_URL_PLACEHOLDER";
 export default API_BASE_URL;
 
