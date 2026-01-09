fc867d6fbcec77882db15ea73ceca6b43f85c632
CloudTechDevOps <147053314+CloudTechDevOps@users.noreply.github.com>
2026-01-09 11:16:43 +0530

    Update config.js

--- client/src/pages/config.js
+++ client/src/pages/config.js
@@ -1,5 +1,5 @@
 //const API_BASE_URL = "/api";  // For reverse proxy it is mandatory so dont change
-const API_BASE_URL = "http://a6f8e23e48ab444cc81c1e5aa30826a7-498822815.us-west-2.elb.amazonaws.com"
+const API_BASE_URL = "http://a033efa1e8b794dbbabf1603dfc2634f-1968123503.us-west-2.elb.amazonaws.com"
 
 // const API_BASE_URL = "http://98.92.112.243:84"
 //const API_BASE_URL = "http://backend.515800584282.realhandsonlabs.net";
