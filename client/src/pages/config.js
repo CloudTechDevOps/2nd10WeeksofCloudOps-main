936c695ccf533c28dc42908546481670b8ab721c
veerababu narni <147053314+CloudTechDevOps@users.noreply.github.com>
2026-05-01 22:30:07 +0530

    Update config.js

--- client/src/pages/config.js
+++ client/src/pages/config.js
@@ -1,5 +1,5 @@
-//const API_BASE_URL = "/api";  // For reverse proxy it is mandatory so dont change
-const API_BASE_URL = "http://34.228.162.240:84"
+const API_BASE_URL = "/api";  // For reverse proxy it is mandatory so dont change
+//const API_BASE_URL = "http://34.228.162.240:84"
 
 // const API_BASE_URL = "http://98.92.112.243:84"
 //const API_BASE_URL = "http://backend.515800584282.realhandsonlabs.net";
