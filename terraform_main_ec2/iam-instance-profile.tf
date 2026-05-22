daa5d640cce19910c56a5efbf7b4428b95d245b1
veerababu narni <147053314+CloudTechDevOps@users.noreply.github.com>
2026-05-22 13:26:04 +0530

    Delete terraform_main_ec2 directory

--- terraform_main_ec2/iam-instance-profile.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_iam_instance_profile" "instance-profile" {
-  name = "naresh-veera-profile"
-  role = aws_iam_role.iam-role.name
-}
--- terraform_main_ec2/outputs.tf
+++
@@ -1,8 +0,0 @@
-output "region" {
-    description = "Jumphost Server region"
-    value = var.region
-}
-output "jumphost_public_ip" {
-  description = "Public IP address of the EC2 jumphost"
-  value       = aws_instance.ec2.public_ip
-}
