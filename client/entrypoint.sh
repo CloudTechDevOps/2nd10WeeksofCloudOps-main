484f6a0b7f418f715c1b95d4e2a23b2e9f05e538
veerababu narni <147053314+CloudTechDevOps@users.noreply.github.com>
2026-06-04 19:07:26 +0530

    Delete client directory

--- client/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Replace the placeholder in JavaScript files with the environment variable value
-find /usr/local/apache2/htdocs -type f -exec sed -i "s|REACT_APP_API_BASE_URL_PLACEHOLDER|${REACT_APP_API_BASE_URL}|g" {} \;
-
-# Start Apache HTTP Server
-httpd-foreground
--- client/src/pages/config.js
+++
@@ -1,9 +0,0 @@
-const API_BASE_URL = "/api";  // For reverse proxy it is mandatory so dont change
-//const API_BASE_URL = "http://54.166.228.85:84"
-
-// const API_BASE_URL = "http://98.92.112.243:84"
-//const API_BASE_URL = "http://backend.515800584282.realhandsonlabs.net";
-// const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://backend";
-//const API_BASE_URL = "REACT_APP_API_BASE_URL_PLACEHOLDER";
-export default API_BASE_URL;
-
